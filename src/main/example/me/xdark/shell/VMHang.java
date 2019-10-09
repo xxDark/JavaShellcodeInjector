bb73ee8dc4b57f1ddaef71e4527f57622b412327
xDark <me@xdark.dev>
2019-10-09 23:22:17 +0300

    Halt => Hang

--- src/main/example/me/xdark/shell/VMHang.java
+++ src/main/example/me/xdark/shell/VMHang.java
@@ -1,12 +1,12 @@
 package me.xdark.shell;
 
-public class VMHalt {
+public class VMHang {
     private static int x1;
 
     public static void main(String[] args) {
         int i = 20000;
         while (i-- > 0) test();
-        ShellcodeRunner.inject(VMHalt.class, "test", "()V", new byte[]{(byte) 0x90, (byte) 0xeb, (byte) 0xfd});
+        ShellcodeRunner.inject(VMHang.class, "test", "()V", new byte[]{(byte) 0x90, (byte) 0xeb, (byte) 0xfd});
         test();
     }
 

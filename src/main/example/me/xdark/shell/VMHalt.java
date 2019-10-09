58e38349bbd9dcabae9afd3b14ed02e48e7c2ae4
xDark <me@xdark.dev>
2019-10-09 23:21:20 +0300

    add another example

--- src/main/example/me/xdark/shell/VMHalt.java
+++ src/main/example/me/xdark/shell/VMHalt.java
@@ -1,13 +1,12 @@
 package me.xdark.shell;
 
-public class Debugger {
-
+public class VMHalt {
     private static int x1;
 
     public static void main(String[] args) {
         int i = 20000;
         while (i-- > 0) test();
-        ShellcodeRunner.inject(Debugger.class, "test", "()V", new byte[]{(byte) 0xCC});
+        ShellcodeRunner.inject(VMHalt.class, "test", "()V", new byte[]{(byte) 0x90, (byte) 0xeb, (byte) 0xfd});
         test();
     }
 
